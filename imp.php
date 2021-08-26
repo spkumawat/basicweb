293e40e82ce00ac4b8744f6d4c2034a29a769bac
spkumawat <89294297+spkumawat@users.noreply.github.com>
2021-08-26 12:10:00 +0530

    Add files via upload

--- imp.php
+++ imp.php
@@ -1,18 +1,17 @@
 <?php
 
 $conn =mysqli_connect("localhost","root","", "student");
- $name = $_POST['name'];
-$password = $_POST['password'];
+ $first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
 
 
-$sql = "INSERT INTO  class(name, password) VALUES ('$name' , '$password')";
+$sql = "INSERT INTO  classs(first_name, last_name) VALUES ('$first_name' , '$last_name')";
 
 if (!mysqli_query($conn,$sql)) {
 echo "data has not saved";
 }
 else {
 echo "data is a saved successfully";
-$stmt ->close();
-$conn ->close();
+
 }
 ?>
